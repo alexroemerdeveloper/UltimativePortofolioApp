9c9b99d9a4c1630a59cb8964e2f5e9c59f288ff9
Alexander Römer <alex@roemer.design>
2020-10-22 18:32:26 +0200

    Initial Commit

---
+++ UltimativePortofolioApp/UltimativePortofolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimativePortofolioAppApp.swift
+//  UltimativePortofolioApp
+//
+//  Created by Alexander Römer on 22.10.20.
+//
+
+import SwiftUI
+
+@main
+struct UltimativePortofolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
