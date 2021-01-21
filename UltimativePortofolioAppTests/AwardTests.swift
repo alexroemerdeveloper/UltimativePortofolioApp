7df08fdd37f39353ef08646f7339625efd89fb73
Alexander Römer <alexroemerdeveloper@gmail.com>
2021-01-21 13:15:34 +0100

    Testing Core Data

---
+++ UltimativePortofolioAppTests/AwardTests.swift
@@ -0,0 +1,72 @@
+//
+//  AwardTests.swift
+//  UltimativePortofolioAppTests
+//
+//  Created by Alexander Römer on 21.01.21.
+//
+
+import XCTest
+import CoreData
+@testable import UltimatePortfolio
+
+class AwardTests: BaseTestCase {
+    
+    let awards = Award.allAwards
+    
+    func testAwardIDMatchesName() {
+        for award in awards {
+            XCTAssertEqual(award.id, award.name, "Award ID should always match its name.")
+        }
+    }
+    
+    func testNoAwards() throws {
+        for award in awards {
+            XCTAssertFalse(dataController.hasEarned(award: award), "New users should have no earned awards")
+        }
+    }
+    
+    func testCompletedAwards() throws {
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+
+        for (count, value) in values.enumerated() {
+            var items = [Item]()
+
+            for _ in 0..<value {
+                let item = Item(context: managedObjectContext)
+                item.completed = true
+                items.append(item)
+            }
+
+            let matches = awards.filter { award in
+                award.criterion == "complete" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1, "Completing \(value) items should unlock \(count + 1) awards.")
+
+            for item in items {
+                dataController.delete(item)
+            }
+        }
+    }
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
