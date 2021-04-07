cc73caa28092a89eab7b3c47b08e8c8975e5869e
Alexander Römer <alexroemerdeveloper@gmail.com>
2021-04-07 20:05:13 +0200

    Offering in-app purchases, part 1

--- UltimativePortofolioApp/UltimativePortofolioAppApp.swift
+++ UltimativePortofolioApp/UltimativePortofolioAppApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct UltimativePortofolioAppApp: App {
     
+    @StateObject var unlockManager: UnlockManager
     @StateObject var dataController: DataController
     
     var body: some Scene {
@@ -17,6 +18,7 @@ struct UltimativePortofolioAppApp: App {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
+                .environmentObject(unlockManager)
                 // Automatically save when we detect that we are
                 // no longer the foreground app. Use this rather than
                 // scene phase so we can port to macOS, where scene
@@ -27,7 +29,10 @@ struct UltimativePortofolioAppApp: App {
     
     init() {
         let dataController = DataController()
+        let unlockManager = UnlockManager(dataController: dataController)
+
         _dataController = StateObject(wrappedValue: dataController)
+        _unlockManager = StateObject(wrappedValue: unlockManager)
     }
     
     func save(_ note: Notification) {
