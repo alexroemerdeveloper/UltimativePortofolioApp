e18e6e701c601aa1385a66bc601f52d54d203c36
Alexander Römer <alexroemerdeveloper@gmail.com>
2021-05-30 20:10:24 +0200

    Creating a second widget

--- PortfolioWidget/PortfolioWidget.swift
+++ PortfolioWidget/PortfolioWidget.swift
@@ -10,6 +10,7 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
+    typealias Entry = SimpleEntry
     
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), items: [Item.example], configuration: ConfigurationIntent())
@@ -28,7 +29,7 @@ struct Provider: IntentTimelineProvider {
     
     func loadItems() -> [Item] {
         let dataController = DataController()
-        let itemRequest = dataController.fetchRequestForTopItems(count: 1)
+        let itemRequest = dataController.fetchRequestForTopItems(count: 5)
         return dataController.results(for: itemRequest)
     }
 }
@@ -38,6 +39,17 @@ struct SimpleEntry: TimelineEntry {
     let items: [Item]
     let configuration: ConfigurationIntent
 }
+
+
+
+@main
+struct PortfolioWidgets: WidgetBundle {
+    var body: some Widget {
+        SimplePortfolioWidget()
+        ComplexPortfolioWidget()
+    }
+}
+
 
 struct PortfolioWidgetEntryView : View {
     var entry: SimpleEntry
@@ -56,9 +68,63 @@ struct PortfolioWidgetEntryView : View {
     }
 }
 
-@main
-struct PortfolioWidget: Widget {
-    let kind: String = "PortfolioWidget"
+struct PortfolioWidgetMultipleEntryView: View {
+    let entry: Provider.Entry
+
+    @Environment(\.widgetFamily) var widgetFamily
+    @Environment(\.sizeCategory) var sizeCategory
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            ForEach(items) { item in
+                HStack {
+                    Color(item.project?.color ?? "Light Blue")
+                        .frame(width: 5)
+                        .clipShape(Capsule())
+
+                    VStack(alignment: .leading) {
+                        Text(item.itemTitle)
+                            .font(.headline)
+                            .layoutPriority(1)
+                        if let projectTitle = item.project?.projectTitle {
+                            Text(projectTitle)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+
+                    Spacer()
+                }
+            }
+        }
+        .padding(20)
+    }
+    
+    var items: ArraySlice<Item> {
+        let itemCount: Int
+
+        switch widgetFamily {
+        case .systemSmall:
+            itemCount = 1
+        case .systemLarge:
+            if sizeCategory < .extraExtraLarge {
+                itemCount = 5
+            } else {
+                itemCount = 4
+            }
+        default:
+            if sizeCategory < .extraLarge {
+                itemCount = 3
+            } else {
+                itemCount = 2
+            }
+        }
+
+        return entry.items.prefix(itemCount)
+    }
+}
+
+struct SimplePortfolioWidget: Widget {
+    let kind: String = "SimplePortfolioWidget"
 
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
@@ -68,6 +134,21 @@ struct PortfolioWidget: Widget {
         .description("This is an example widget.")
     }
 }
+
+
+struct ComplexPortfolioWidget: Widget {
+    let kind: String = "ComplexPortfolioWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            PortfolioWidgetMultipleEntryView(entry: entry)
+        }
+        .configurationDisplayName("Up next…")
+        .description("Your most important items.")
+    }
+}
+
+
 
 struct PortfolioWidget_Previews: PreviewProvider {
     static var previews: some View {
